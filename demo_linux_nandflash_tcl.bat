29e0a0dd9c603f0462ffea7685d89be092b46d14
root <root@SEL-LT-A41839B.mchp-main.com>
2026-08-27 13:43:19 +0900

    chore: clear all contents (superseded by local OneDrive archive)

--- demo_linux_nandflash_tcl.bat
+++
@@ -1,2 +0,0 @@
-sam-ba.exe \usb\ARM0 at91sama5d3x-ek demo_linux_nandflash.tcl > logfile.log 2>&1
-notepad logfile.log
--- kernel_update.bat
+++
@@ -1,3 +0,0 @@
-copy \\wsl.localhost\Ubuntu-24.04\root\build\sama5d3_xplained.itb sama5d3_xplained-wsl.itb
-sam-ba.exe \usb\ARM0 at91sama5d3x-ek kernel_update.tcl > logfile_kernel.log 2>&1
-notepad logfile_kernel.log
